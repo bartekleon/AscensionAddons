6256afc9872353bc4e08dbe878137ace64958a5e
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-04-06 19:12:06 +0300

    fix

--- SellPoorItems/SellPoorItems.lua
+++ SellPoorItems/SellPoorItems.lua
@@ -5,8 +5,11 @@ local function EventHandler(self, event)
       if caslots ~= nil then
         for slot = 1, caslots do
           local item = GetContainerItemLink(bag, slot)
-          if item and GetItemInfo(item)[3] == 0 then
-            UseContainerItem(bag, slot)
+          if item then
+            _, _, quality = GetItemInfo(item)
+            if quality == 0 then
+              UseContainerItem(bag, slot)
+            end
           end
         end
       end
